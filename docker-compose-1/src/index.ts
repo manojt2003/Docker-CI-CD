d7220cad0ad4dd062c8e7877455600afb1ad78f7
unknown <tmanojhonnali@gmail.com>
2026-05-17 18:03:49 +0530

    docker compose-1

---
+++ docker-compose-1/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express"
+
+let app = express()
+
+import "dotenv/config";
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+app.post("/", (req, res) => {
+    let sUser = prisma.user.create({
+        data: {
+            username: Math.random().toString(),
+            password: Math.random().toString()
+        }
+    })
+
+    res.json({"message": "signin successfully "})
+
+})
+
+app.get("/", (req, res) => {
+    let lUser = prisma.user.findMany()
+    
+    res.json({lUser})
+})
+
+app.listen(3000)
